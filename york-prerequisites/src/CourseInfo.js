7ddb92d6bcd8cadc3b39db3c443a5e31517ad9ea
unknown <jorra04@my.yorku.ca>
2021-07-26 12:50:58 -0400

    Fixed the issue with the styling

--- york-prerequisites/src/CourseInfo.js
+++ york-prerequisites/src/CourseInfo.js
@@ -1,13 +1,27 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import "./CourseInfo.css"
 const CourseInfo = ({ course_id, prerequisites }) => {
+
+    const [hasPrerequisites, setHasPrerequisites] = useState(false);
+
+
+    useEffect(() => {
+        setHasPrerequisites(!(prerequisites === undefined || prerequisites.length === 0))
+      }, []);
+
+    
+
+
     return (
         <div className="course">
             <h1 className="course-id">{course_id}</h1>
-            <p className="prerequisites">Prerequisites: {prerequisites.join(', ')}</p>
+            {hasPrerequisites ? <p className="prerequisites">Prerequisites: {prerequisites.join(', ')}</p>  : <p className="prerequisites">No Prerequisites</p>}
         </div>
     );
   };
 
 
 export default CourseInfo;
+
+
+//<p className="prerequisites">Prerequisites: {prerequisites.join(', ')}</p>
--- york-prerequisites/src/Courses.js
+++ york-prerequisites/src/Courses.js
@@ -1,43 +1,60 @@
 import React, { useEffect, useState } from "react";
 import CourseInfo from "./CourseInfo";
-import "./Courses.css"
+import "./Courses.css";
 
 const Teams = () => {
   const exampleReq = `http://yorkapi-env.eba-fi5ekpb4.us-east-2.elasticbeanstalk.com/courses`;
   const [teams, setTeams] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [teamsOriginal, setTeamsOriginal] = useState([]);
 
-  useEffect(async () => {
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+
+    const getData = async () => {
+      const resp = await fetch(exampleReq);
+      const data = await resp.json();
+      setTeams(data.courses);
+      setTeamsOriginal(data.courses);
+    };
+
+
     getData();
   }, []);
 
-  const getData = async () => {
-    const resp = await fetch(exampleReq);
-    const data = await resp.json();
-    setTeams(data.courses);
-    setLoading(false);
+  const updateSearch = (e) => {
+    setSearchText(e.target.value);
+    let newTeams = teamsOriginal.filter((team) => {
+      return team.course_id.toLowerCase().includes(e.target.value);
+    });
+
+    setTeams(newTeams);
   };
+
+  
 
   return (
     <div className="App">
-        big guy
-    
+      <div className="search">
+        <input
+          className="search-bar"
+          type="text"
+          value={searchText}
+          onChange={updateSearch}
+        />
+      </div>
 
-
-    <div className="courses">
-
-    
-    {teams.map((team) => (
-        <CourseInfo key={team._id}
-        course_id={team.course_id}
-        prerequisites={team.prerequisites}/>
-    ))}
-    </div>
+      <div className="courses">
+        {teams.map((team) => (
+          <CourseInfo
+            key={team._id}
+            course_id={team.course_id}
+            prerequisites={team.prerequisites}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
-
-
 export default Teams;
-
