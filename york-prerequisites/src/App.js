301f40fcfaec8f972e9413d392d6551cb7590213
jorra04 <jorra04@my.yorku.ca>
2021-07-27 01:00:19 -0400

    Fixed footer

--- york-prerequisites/src/App.js
+++ york-prerequisites/src/App.js
@@ -3,31 +3,29 @@ import Courses from "./Courses";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Contact from "./components/Contact"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="page-container">
-    <div className="content-wrap">
     <Router>
-      
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Courses}/>
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
-        </Switch>
-        
-      
-    </Router>
-    </div>
-    <Footer />
-    </div>
-  );
-}
+      <div className="page-container">
+        <div className="content-wrap">
 
-const Contact = () => {
-  return (
-    <div>hi there</div>
+
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Courses} />
+            <Route path="/about" exact component={About} />
+            <Route path="/contact" exact component={Contact} />
+          </Switch>
+
+
+
+        </div>
+        <Footer />
+
+      </div>
+    </Router>
   );
 }
 export default App;
--- york-prerequisites/src/components/Footer.js
+++ york-prerequisites/src/components/Footer.js
@@ -1,43 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 const Footer = () => {
   return (
-    <div className="main-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h4>Jorra Inc</h4>
-            <ul className="list-unstyled">
-              <li>321-333-4421</li>
-              <li>Vaughan, ON</li>
-              <li>123 Street St</li>
-            </ul>
+
+    <>
+      <br></br>
+      <br></br>
+      <div className="main-footer">
+        <div className="container">
+          <div className="row">
+            <p className="col-sm">
+              &copy;{new Date().getFullYear()} Jorra Singh | All rights reserved | {}
+              <Link className="footer-links" to="/contact">Terms Of Service</Link> | <Link className="footer-links" to="/">Privacy</Link>
+            </p>
           </div>
-          <div className="col">
-            <h4>Other Stuff</h4>
-            <ul className="list-unstyled">
-              <li>321-333-4421</li>
-              <li>Vaughan, ON</li>
-              <li>123 Street St</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h4>Other Stuff pt2</h4>
-            <ul className="list-unstyled">
-              <li>321-333-4421</li>
-              <li>Vaughan, ON</li>
-              <li>123 Street St</li>
-            </ul>
-          </div>
-        </div>
-        <div className="row">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} Jorra Singh | All rights reserved |
-            Terms Of Service | Privacy
-          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
