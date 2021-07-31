8fdaf0a5c1229afe02225892efd314bd9267c674
unknown <jorra04@my.yorku.ca>
2021-07-31 16:44:11 -0400

    Fixed background issue. Updated 404 page.

--- york-prerequisites/src/App.js
+++ york-prerequisites/src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import TermsOfService from "./components/TermsOfService";
 import Privacy from "./components/Privacy";
-import gif_404 from "./404.png";
+import image_404 from "./404.png";
 import {
   BrowserRouter as Router,
   Switch,
@@ -39,9 +39,12 @@ const My404Component = () => {
   return (
       
       <div className="unknown-404-container">
+        <div className="unknown-404-text-container">
+          <h1 className="unknown-404-text-header">404 Uncharted Territory</h1>
+          <p className="unknown-404-text-paragraph">The page you are looking for does not exist!</p>
+        </div>
         <div className="unknown-404-image-container">
-          <h1></h1>
-          <img className="unknown-404-image" src={gif_404}></img>
+          <img className="unknown-404-image" src={image_404}></img>
         </div>
       </div>
   );
