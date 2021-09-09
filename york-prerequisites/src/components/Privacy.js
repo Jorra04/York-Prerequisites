23f4f0e009c0efcb67690d09c0937396750e3ed1
jorra04 <jorra04@my.yorku.ca>
2021-09-09 16:32:09 -0400

    All changes are done, need to take care of the pagination for the search and then I can begin thinking about deploying.

--- york-prerequisites/src/components/Privacy.js
+++ york-prerequisites/src/components/Privacy.js
@@ -11,7 +11,7 @@ const Privacy = () => {
         <br />
         <div className="privacy-paragraphs">
           <img className="privacy-image" src={privacyLock}/>
-          <br/>
+
           <div className="privacy-first-paragraph">
             <p>This application is <span className="privacy-paragraph-bold">does not</span> collect your information. This is simply a personal project that is designed to aid
             in the selection of courses for students (not intended to be the sole resource, see Terms of Service).
