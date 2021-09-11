b105d37d575942268d0f3eabb399b78ccbf74f19
unknown <jorra04@my.yorku.ca>
2021-09-11 14:11:06 -0400

    Added pagination and styles for the paging system.

--- src/Courses.js
+++ src/Courses.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import CourseInfo from "./CourseInfo";
 import "./Courses.css";
 import { Link } from "react-router-dom";
+import Pagination from "./Pagination";
 
 const Teams = () => {
   const request = `https://prerequisitechecker-york.herokuapp.com/courses`;
   const [courses, setCourses] = useState([]);
   const [allCourses, setAllCourses] = useState([]);
   const [searchText, setSearchText] = useState("");
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [coursesPerPage] = useState(8);
 
   const [firstSearch, setFirstSearch] = useState(true);
 
@@ -26,25 +28,35 @@ const Teams = () => {
   };
 
   const performSearch = () => {
-    if(firstSearch) {
+    if (firstSearch) {
       setFirstSearch(false);
     }
     let tempCourses = [];
-    if(searchText.length !== 0) {
+    if (searchText.length !== 0) {
       tempCourses = allCourses.filter((course) => {
-        return course.course_id.toLowerCase().includes(searchText.toLowerCase());
+        return course.course_id
+          .toLowerCase()
+          .includes(searchText.toLowerCase());
       });
-      
-    }
-    
-    setCourses(tempCourses);
 
+      setCurrentPage(1);
+    }
+
+    setCourses(tempCourses);
   };
 
   const keyPressed = (e) => {
     if (e.key === "Enter" || e.keyCode === 13) {
       performSearch();
     }
+  };
+
+  const indexOfLastPost = currentPage * coursesPerPage;
+  const indexOfFirstPost = indexOfLastPost - coursesPerPage;
+  const currentPosts = courses.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
   return (
@@ -59,30 +71,52 @@ const Teams = () => {
           onChange={updateSearch}
           onKeyDown={keyPressed}
         />
-        <button className="search-button" onClick={performSearch}>Search</button>
+        <button className="search-button" onClick={performSearch}>
+          Search
+        </button>
       </div>
-      <div className="courses">
-        { courses.length !== 0 ? <FoundCourses courses={courses}/> : !firstSearch ? <NoCoursesFound/> : <></> }
+      <div>
+        {courses.length !== 0 ? (
+          <>
+            <div className="courses">
+              <FoundCourses courses={currentPosts} />
+            </div>
+            <div className="pagination-pages">
+              <Pagination
+                postsPerPage={coursesPerPage}
+                totalPosts={courses.length}
+                currentPage={currentPage}
+                paginate={paginate}
+              />
+            </div>
+          </>
+        ) : !firstSearch ? (
+          <NoCoursesFound />
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
 };
 
-
-
-const FoundCourses = ({courses}) => {
-  return (
-    courses.map((course) => (
-      <Link className="coursePage" to={`/courseDetail/${course.course_id.replace(/[0-9 ]/g, '')}/${course.course_id.replace(/[a-zA-z/ ]/g, '')}`}>
-        <CourseInfo
+const FoundCourses = ({ courses }) => {
+  return courses.map((course) => (
+    <Link
+      className="coursePage"
+      to={`/courseDetail/${course.course_id.replace(
+        /[0-9 ]/g,
+        ""
+      )}/${course.course_id.replace(/[a-zA-z/ ]/g, "")}`}
+    >
+      <CourseInfo
         key={course._id}
         course_id={course.course_id}
         prerequisites={course.prerequisites}
-        />
-      </Link>
-    ))
-  );
-}
+      />
+    </Link>
+  ));
+};
 
 const NoCoursesFound = () => {
   return (
@@ -95,6 +129,6 @@ const NoCoursesFound = () => {
       </h4>
     </div>
   );
-}
+};
 
 export default Teams;
